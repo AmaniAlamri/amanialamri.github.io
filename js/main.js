e770beeff6442fabd811c0eb91ed91ab79f79007
amani11 <amani.g.66@gmail.com>
2019-03-13 11:29:13 +0300

    alerts popup window has been changed

--- js/main.js
+++ js/main.js
@@ -6,12 +6,10 @@ arrayOfeResult.length = 9; //create array of 9 empty elements
 var countClick = 0; // to count the number of clicks
 var index = 0; // to get the id of clicked div and use it as index of arrayOfeResult
 var tem = 0;
-var winner=false;; //used to check if there is a winner or not
-var play;
+var winner = false; //used to check if there is a winner or not
 var xScore = 0;
 var oScore = 0;
-var turn=$("#turn");
-var test;
+var turn = $("#turn");
 
 var xo = $(".box").on("click", function(event) {
   var target = $(event.target);
@@ -19,20 +17,19 @@ var xo = $(".box").on("click", function(event) {
   index = tem;
   tem = 0;
 
-
   if (target.css("background-image") == "none") {
-    
     if (offX != true) {
       turn.text("O turn");
       target.css({
         "background-image": "url('images/x.png')",
         "background-size": "contain"
       });
-      
+
       offX = true; // to stop player x
       //(target).off("click"); //to prevent player from click on already clicked div
       arrayOfeResult[index] = 1; //insert 1 to the array of result to represent x
       countClick++;
+      console.log(countClick);
 
       // turn of O player
     } else {
@@ -41,11 +38,12 @@ var xo = $(".box").on("click", function(event) {
         "background-image": "url('images/o.png')",
         "background-size": "contain"
       });
-      
+
       offX = false; //to allow plater x
       //(target).off("click"); //to prevent player from click on already clicked div
       arrayOfeResult[index] = 0; ////insert 0 to array of result to represent 0
       countClick++;
+      console.log(countClick);
     }
 
     $("#play").on("click", function() {
@@ -55,6 +53,8 @@ var xo = $(".box").on("click", function(event) {
       offX = true; //turn to o
       index = 0;
       result = 0;
+      winner = false;
+      turn.text("Start Game");
     });
 
     function finishRound() {
@@ -64,7 +64,8 @@ var xo = $(".box").on("click", function(event) {
       offX = true; //turn to o
       index = 0;
       result = 0;
-      $('.i').css("visibility","hidden");
+      winner = false;
+      turn.text("Game Over");
     }
 
     //determine different path
@@ -73,33 +74,23 @@ var xo = $(".box").on("click", function(event) {
 
       // .....row path
       var path1 = arrayOfeResult[0] + arrayOfeResult[1] + arrayOfeResult[2];
-      //console.log("path1" + path1);
       var path2 = arrayOfeResult[3] + arrayOfeResult[4] + arrayOfeResult[5];
-      //console.log("path2" + path2);
       var path3 = arrayOfeResult[6] + arrayOfeResult[7] + arrayOfeResult[8];
-      //console.log("path3" + path3);
 
       // .... column path
       var path4 = arrayOfeResult[0] + arrayOfeResult[3] + arrayOfeResult[6];
-      //console.log("path4" + path4);
       var path5 = arrayOfeResult[1] + arrayOfeResult[4] + arrayOfeResult[7];
-      //console.log("path5" + path5);
       var path6 = arrayOfeResult[2] + arrayOfeResult[5] + arrayOfeResult[8];
-      //console.log("path6" + path6);
 
       // .... diagonal path
       var path7 = arrayOfeResult[0] + arrayOfeResult[4] + arrayOfeResult[8];
-      //console.log("path7" + path7);
       var path8 = arrayOfeResult[2] + arrayOfeResult[4] + arrayOfeResult[6];
-      //console.log("path8" + path8);
 
       for (var i = 1; i <= 8; i++) {
         var result = eval(`path${i}`); // trying to evaluates or executes an argument.
-        //console.log(result+'I'm result '+ typeof s);
 
         if (!isNaN(result)) {
           if (result == 3) {
-            console.log("x winner");
             swal("Good job!", " X winner!", "success", {
               button: "Aww yiss!"
             }).then(() => {
@@ -114,7 +105,6 @@ var xo = $(".box").on("click", function(event) {
           }
           if (result == 0) {
             stopPlay = true;
-            console.log("o winner");
             swal("Good job!", "O winner!", "success", {
               button: "Aww yiss!"
             }).then(() => {
@@ -130,24 +120,22 @@ var xo = $(".box").on("click", function(event) {
         }
       }
       // ..... No winner..................
-      if (countClick > 8 && winner != true) {
-        console.log($(".i"));
-        swal("Draw !!", "...Try agin!", {}).then(() => {
+      if (countClick == 9 && winner != true) {
+        swal(
+          {
+           title: "Draw !!", 
+           text:"...Try agin!",
+           icon:"" 
+          }).then(() => {
           finishRound();
         });
-        $('.i').css("visibility","visible");
-        console.log("Draw !!");
+        $("#play").removeClass("bounce");
         // //-----------------------
       }
     } //close (if click>4)
   }
-
 }); // close click function
 
-// var xo = $(".box").on("mouseenter", function (event) {
-//   var target=$(event.target);
-//   target.css("hidden","visible")
-// });
 // **......My challenges........**
 // 1- link jq script file to html
 // 2- switch between two players
